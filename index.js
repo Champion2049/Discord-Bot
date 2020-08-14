121cc52fc2e39d86a0c00d2b00fc80afbe29bd57
Champion2049 <me.chirayu.6@gmail.com>
2020-08-14 13:08:26 +0530

    hi

--- index.js
+++ index.js
@@ -765,5 +765,30 @@ client.on('message', async message => {
                           });
           }  
 });
+client.on('message', async message => {
+  if(!message.content.startsWith(prefix) || message.author.bot) return;
+       const args = message.content.slice(prefix.length).split(/ +/);
+       const command = args.shift().toLowerCase();
+  if (command === 'etest') {
+                  message.reply('The bot will now shut down.\n'
+                          + 'Confirm with a thumb up or deny with a thumb down.');
+                  message.react('👍').then(r => {
+                          message.react('👎');
+                  });
+                  message.awaitReactions((reaction, user) => user.id === message.author.id && (reaction.emoji.name === '👍' || reaction.emoji.name === '👎'),
+                          { max: 1, time: 30000 }).then(collected => {
+                                  if (collected.first().emoji.name === '👍') {
+                                          message.reply('Shutting down...');
+                                          client.destroy();
+                                  }
+                                  else
+                                          message.reply('Operation canceled.');
+                          }).catch(() => {
+                                  message.reply('No reaction after 30 seconds, operation canceled');
+                          });
+
+
+          }  
+});
 
 client.login('NzMwNjQ0MzQ5ODk3MDE1MzA3.Xwafkw.wFHybJO8bgC45AC8y7GbKT3-mD0');
