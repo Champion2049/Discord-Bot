af960735898aa41b77d906931d261fdd32be32ca
Champion2049 <me.chirayu.6@gmail.com>
2020-09-28 11:28:56 +0530

    Update index.js

--- index.js
+++ index.js
@@ -470,9 +470,7 @@ client.on('message', async message => {
                                     m.reactions.removeAll()
                                   }
                                   else if(collected.first().emoji.name === '❌'){
-                                    const c = new Discord.MessageEmbed()
-                                    m.edit(c);
-                                    m.reactions.removeAll()
+                                    m.delete();
                                   }
                                   else
                                           message.reply('Operation canceled.');
