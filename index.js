a5d5de08fea6207baedc1937671de459e02340ee
Champion2049 <me.chirayu.6@gmail.com>
2020-09-28 12:16:55 +0530

    Update index.js

--- index.js
+++ index.js
@@ -470,7 +470,7 @@ client.on('message', async message => {
                                     m.reactions.removeAll()
                                   }
                                   else if(collected.first().emoji.name === '❌'){
-                                    m.edit('Closed').then(msg => msg.delete({timeout: 5000}));
+                                    m.edit('Closing....').then(msg => msg.delete({timeout: 5000}));
                                   }
                                   else
                                           message.reply('Operation canceled.');
