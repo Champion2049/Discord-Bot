20201a4cb952f7fdd830f5b9fb0e861fa2034a97
Champion2049 <me.chirayu.6@gmail.com>
2020-09-28 11:11:02 +0530

    Update index.js

--- index.js
+++ index.js
@@ -268,7 +268,7 @@ message.delete()
 if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("You don't have premssions to do that!");
 if(!args[0]) return message.channel.send('Please enter a number of messages to clear! `Usage: dcclear <amount>`');
 message.channel.bulkDelete(args[0]).then(() => {
-message.channel.send(`**__Cleared ${args[0]} messages.__**`).then(message => message.delete(2000));
+message.channel.send(`**__Cleared ${args[0]} messages.__**`).then(msg => msg.delete({ timeout: 3000 }));
 })
        }
 });
