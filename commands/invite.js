af53031c084154fa19b30fb1cd62dac3592aa501
Champion2049 <me.chirayu.6@gmail.com>
2020-11-01 15:58:43 +0530

    Update invite.js

--- commands/invite.js
+++ commands/invite.js
@@ -19,6 +19,7 @@ module.exports = {
       .addField(`Invite the bot from Bots for Discord`, `https://botsfordiscord.com/bot/730644349897015307`)
       .addField(`Invite the bot from Aspect Bot List`, `https://sudden-tidal-phalange.glitch.me/bots/730644349897015307`)
       .addField(`Invite the bot from Botrix`, `https://botrix.cc/bots/730644349897015307`)
+      .addField(`Invite the bot from Top.gg`, `https://top.gg/bot/730644349897015307`)
       .setColor("BLUE")
       .setFooter("Bot made by Champion2049#3714", 'https://cdn.discordapp.com/avatars/730644349897015307/6eff6602ff525e3170f13444942fcba0.png?size=256')
       message.channel.send(embed)
