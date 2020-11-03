806df5207eb83311109412fecb2320253a24a975
Champion2049 <me.chirayu.6@gmail.com>
2020-11-03 13:02:43 +0530

    Update index.js

--- index.js
+++ index.js
@@ -780,22 +780,4 @@ if (badwordlist.some((word) => message.content.toLowerCase().includes(word))) {
   message.channel.send(`That word is not allowed here!`)
 }
 })
-client.on('message',message => {
-const prefix = db.get(`guild_${message.guild.id}_prefix`) || "dc"
-            if(!message.content.startsWith(prefix) || message.author.bot) return;
-            const args = message.content.slice(prefix.length).split(/ +/);
-            const command = args.shift().toLowerCase();
-          if (command === 'unmute') {
-              if(!args[0]) return message.channel.send("Please mention the member to unmute!")
-              if(!args[0] == message.guild.members) return message.channel.send("That member is not in the server!")
-              if(!message.member.hasPermission('MUTE_MEMBERS')) return message.channel.send("You don't have the permission to advocate this command")
-              if(!message.guild.me.hasPermission("MUTE_MEMBERS")) return message.channel.send("I don't have the permission to advocate this command")
-              let muterole = message.guild.roles.cache.find(muterole => muterole.name === "Muted");
-              let user = message.mentions.users.first();
-              if (!user.roles.has(muterole)) return message.reply(`That member is :no_entry_sign: muted!`);
-              else {user.removeRole(muterole) 
-                return message.channel.send(`${user} has been unmuted!`)}
-          
-          }
-        })
 client.login('NzMwNjQ0MzQ5ODk3MDE1MzA3.Xwafkw.wFHybJO8bgC45AC8y7GbKT3-mD0');
