ecfa4ac13be72bd4611fbab4064ebde6edd55987
Champion2049 <me.chirayu.6@gmail.com>
2020-11-20 09:48:39 +0530

    Update gif.js

--- commands/gif.js
+++ commands/gif.js
@@ -31,14 +31,14 @@ module.exports = {
       "timestamp": new Date(),
       "footer": {
         "icon_url": "https://raw.githubusercontent.com/Giphy/GiphyAPI/f68a8f1663f29dd9e8e4ea728421eb2977e42d83/api_giphy_logo_sparkle_clear.gif",
-        "text": "Powered by Giphy"
+        "text": `Gif requested by ${message.author.username} || Powered by Giphy`
       },
       "image": {
         "url": msgurl
       },
       "fields": [
         {
-          "name": "Search Term",
+          "name": `Search Term`,
           "value": "`" + term + "`",
           "inline": true
         },
