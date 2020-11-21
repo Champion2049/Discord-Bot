b009fcbbf1f3e0421f9b2b4b65637fe8f1148327
Champion2049 <me.chirayu.6@gmail.com>
2020-11-21 15:42:45 +0530

    first

--- index.js
+++ index.js
@@ -793,4 +793,20 @@ if (command === 'serverlist') {
   })      
 }
 })
+client.on('message', async message => {
+  const prefix = db1.get(`guild_${message.guild.id}_prefix`) || "dc"
+  if(!message.content.startsWith(prefix) || message.author.bot) return;
+  const args = message.content.slice(prefix.length).split(/ +/);
+  const command = args.shift().toLowerCase();
+if (command === 'fortniteshop') {
+const canvas = require("discord-canvas"),
+   shop = new canvas.FortniteShop();
+   const msg = await message.channel.send("Fetching the shop...")
+const image = await shop
+  .setToken("185bdd76-87b9-41ad-9d55-b86599679d29")
+  .toAttachment();
+const attachment = new Discord.MessageAttachment(image, "FortniteShop.png");
+message.channel.send(attachment) && msg.delete()
+}
+})
 client.login('NzMwNjQ0MzQ5ODk3MDE1MzA3.Xwafkw.wFHybJO8bgC45AC8y7GbKT3-mD0');
